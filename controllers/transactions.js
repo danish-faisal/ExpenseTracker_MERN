228d3afccd714d4dac2a16067793ace5cbf23f8d
= <danishfaisal.wwe@gmail.com>
2022-02-13 16:40:21 +0530

    APIs initialised

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,6 +1,20 @@
 // @desc        Get all transactions
 // @routes      GET /api/v1/transactions
 // @access      Public
-exports.getTransactions = (req, res) => {
-    return res.send('GET Transactions');
+exports.getTransactions = (req, res, next) => {
+    res.send('GET Transactions');
+}
+
+// @desc        Add transactions
+// @routes      POST /api/v1/transactions
+// @access      Public
+exports.addTransaction = (req, res, next) => {
+    res.send('POST Transaction');
+}
+
+// @desc        Delete transactions
+// @routes      DELETE /api/v1/transactions/:id
+// @access      Public
+exports.deleteTransaction = (req, res, next) => {
+    res.send('DELETE Transaction');
 }
