2fece5ce6a5ad3028e6a1eedc2700cad31aeb053
= <danishfaisal.wwe@gmail.com>
2022-02-13 20:15:17 +0530

    Making the App - Production Ready

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const path = require('path');
 
 dotenv.config({ path: './config/config.env' });
 
@@ -20,6 +21,12 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use('/api/v1/transactions', transactions);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 const PORT = process.env.PORT || 5000;
 
