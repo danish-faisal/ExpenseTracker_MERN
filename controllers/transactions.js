f82d7a716f91bd4cd021893a14db34cb0c5b66d5
= <danishfaisal.wwe@gmail.com>
2022-02-13 18:04:38 +0530

    Get all transactions API function

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,20 +1,35 @@
+const Transaction = require('../models/Transaction');
+
 // @desc        Get all transactions
 // @routes      GET /api/v1/transactions
 // @access      Public
-exports.getTransactions = (req, res, next) => {
-    res.send('GET Transactions');
+exports.getTransactions = async (req, res, next) => {
+    try {
+        const transactions = await Transaction.find();
+
+        return res.status(200).send({
+            success: true,
+            count: transactions.length,
+            data: transactions
+        });
+    } catch (err) {
+        return res.status(500).send({
+            success: false,
+            error: 'Server error'
+        });
+    }
 }
 
 // @desc        Add transactions
 // @routes      POST /api/v1/transactions
 // @access      Public
-exports.addTransaction = (req, res, next) => {
+exports.addTransaction = async (req, res, next) => {
     res.send('POST Transaction');
 }
 
 // @desc        Delete transactions
 // @routes      DELETE /api/v1/transactions/:id
 // @access      Public
-exports.deleteTransaction = (req, res, next) => {
+exports.deleteTransaction = async (req, res, next) => {
     res.send('DELETE Transaction');
 }
