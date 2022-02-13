da1efc976155251a008b68e0d944703aadf03639
= <danishfaisal.wwe@gmail.com>
2022-02-13 13:31:45 +0530

    Folder structure set

---
+++ controllers/transactions.js
@@ -0,0 +1,6 @@
+// @desc        Get all transactions
+// @routes      GET /api/v1/transactions
+// @access      Public
+exports.getTransactions = (req, res) => {
+    return res.send('GET Transactions');
+}
