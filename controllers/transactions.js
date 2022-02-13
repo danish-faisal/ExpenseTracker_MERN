abd7a4ff9ddf19ba778e8627c8fbdbc43c86a723
= <danishfaisal.wwe@gmail.com>
2022-02-13 18:22:33 +0530

    Add transaction API function

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -7,15 +7,15 @@ exports.getTransactions = async (req, res, next) => {
     try {
         const transactions = await Transaction.find();
 
-        return res.status(200).send({
+        return res.status(200).json({
             success: true,
             count: transactions.length,
             data: transactions
         });
     } catch (err) {
-        return res.status(500).send({
+        return res.status(500).json({
             success: false,
-            error: 'Server error'
+            error: 'Server Error'
         });
     }
 }
@@ -24,7 +24,28 @@ exports.getTransactions = async (req, res, next) => {
 // @routes      POST /api/v1/transactions
 // @access      Public
 exports.addTransaction = async (req, res, next) => {
-    res.send('POST Transaction');
+    try {
+        const transaction = await Transaction.create(req.body);
+
+        return res.status(201).json({
+            success: true,
+            data: transaction
+        });
+    } catch (err) {
+        if (err.name == 'ValidationError') {
+            const errors = Object.values(err.errors).map(val => val.message);
+
+            return res.status(400).json({
+                success: false,
+                errors: errors
+            });
+        } else {
+            return res.status(500).json({
+                success: false,
+                error: 'Server Error'
+            });
+        }
+    }
 }
 
 // @desc        Delete transactions
